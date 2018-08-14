61d9277d11e5c65c2e8b187960681b5a0491d123
Goran Maksimovski <goran.maksimovski@keitaro.com>
2018-08-14 14:18:53 +0200

    Restrict anon access to user_show

--- ckanext/perth/logic/action/get.py
+++ ckanext/perth/logic/action/get.py
@@ -12,3 +12,16 @@ def user_list(context, data_dict):
     '''
     _check_access('sysadmin', context, data_dict)
     return logic.action.get.user_list(context, data_dict)
+
+
+@toolkit.side_effect_free
+def user_show(context, data_dict):
+    '''Forbid anonymous access to user info.
+    API call works with POST request with authorization header and id
+    of desired user supplied.
+    '''
+    if context.get('user'):
+        return logic.action.get.user_show(context, data_dict)
+    else:
+        raise toolkit.NotAuthorized(
+            'You must be logged in to perform this action.')
